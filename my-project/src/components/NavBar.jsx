ea9699341f00ffbb9466b1ba1a8fd4b2d58d6e6a
Muksiduz <muksiduzzaman124@gmail.com>
2026-03-14 00:22:37 +0530

    logo updated

--- my-project/src/components/NavBar.jsx
+++ my-project/src/components/NavBar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
           initial={{ opacity: 0, y: -10 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6 }}
-          className="h-20 w-auto"
+          className="h-20 w-auto rounded-full"
         />
 
         {/* Desktop Links */}
